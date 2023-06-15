18af75d6cbf2383e1100f9bee98dc154a4fad891
antara biswas <136632927+antara-123@users.noreply.github.com>
2023-06-15 11:03:10 +0530

    Add files via upload
    project-3

---
+++ newproject/js/newscript.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2500,
+    dots:true,
+    items:1,
+    animateOut:'fadeOut'
+
+})
